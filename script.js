02bd1d5cd12ac54bc16bb262377a3df748562df2
Larry Cessna <larrybcessna@gmail.com>
2020-10-04 14:07:00 -0700

    questions and answers now append to page.

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var score = 0;
 var welcomeEL = document.querySelector(".welcome");
 var startEL = document.getElementById("start");
 var timeEl = document.querySelector(".countdown");
-var mainEl = document.getElementById("main");
+var questionsEl = document.getElementById("questions");
+var choicesEl = document.getElementById("choices");
+
+var currentQuestion = 0;
 
 welcomeEL.textContent = "Press this button to start the quiz!";
 
 startEL.addEventListener("click", countingDown);
-
 
 function countingDown() {
     var timeLeft = 5;
@@ -22,39 +24,99 @@ function countingDown() {
             quiz();
         }
 
-    }, 1000);
+    }, 100);
 }
-
 
 function quiz() {
+    var quizTimeLeft = 90;
     welcomeEL.textContent = "The quiz has begun!"
-    timeEl.textContent = " ";
-
-
+    askNextQuestion();
+    var quizInterval = setInterval(function () {
+        timeEl.textContent = quizTimeLeft + " seconds remaining";
+        quizTimeLeft--;
+    }, 1000)
 }
 
-var questions = [ {
-    question: "Who created JavaScript?",
-    choices: ["Microsoft", "Sun Microsystems","Oracle", "Netscape"],
-    correctAnswer: 3
-},
-{
-    question: "Who invented JavaScript?",
-    choices: ["Douglas Crockford", "Sheryl Sandberg","Brendan Eich", "Larry Cessna"],
-    correctAnswer: 2
-},
-{
-    question: "How long did Brendan Eich take to write the JavaScript language?",
-    choices: ["10 days", "2 weeks", "2 months","10 months"],
-    correctAnswer: 0
-},
-{
-    question: "JavaScript wasn't always called that. What other name has it been released under?",
-    choices: ["Latte", "Mocha", "BScript", "Spidermonkey"],
-    correctAnswer: 1
-},
-{
-    question: "Which tool can you use to ensure code quality?",
-    choices: ["Angular", "jQuery", "RequireJS", "ESLint"],
-    correctAnswer: 3
-}]
+function askNextQuestion() {
+    questionsEl.textContent = questions[currentQuestion].question
+    let choices = questions[currentQuestion].choices;
+
+    //list all choices as li under current question
+    for (let j = 0; j < choices.length; j++) {
+        const element = choices[j];
+        var node = document.createElement("li");
+        var textnode = document.createTextNode(element.text);
+        node.appendChild(textnode);
+        choicesEl.appendChild(node);
+    }
+   };
+
+//ask 1st question
+//list choices of 1st question
+//stop until a choice is made
+//add eventlistener to wait for a choice
+//once a choice is made, increment currentQuestion++
+//ask 2nd question
+//so on...
+//quiz stops after currentQuestion.length is reached.
+
+
+    var questions = [{
+        question: "Which company created JavaScript?",
+        choices: [
+            {
+                text: "Microsoft", correct: "false"
+            },
+            {
+                text: "Sun Microsystems", correct: "false"
+            },
+            {
+                text: "Oracle", correct: "true"
+            },
+            {
+                text: "Netscape", correct: "false"
+            }],
+    },
+    {
+        question: "Who invented JavaScript?",
+        choices: [
+            {
+                text: "Douglas Crockford", correct: "false"
+            },
+            {
+                text: "Sheryl Sandberg", correct: "false"
+            },
+            {
+                text: "Brendan Eich", correct: "true"
+            },
+            {
+                text: "David Boucher", correct: "false"
+            }],
+
+    },
+    {
+        question: "How long did Brendan Eich take to write the JavaScript language?",
+        choices: [
+            {
+                text: "10 days", correct: "true"
+            },
+            {
+                text: "2 weeks", correct: "false"
+            },
+            {
+                text: "2 months", correct: "false"
+            },
+            {
+                text: "10 months", correct: "false"
+            }],
+    },
+    {
+        question: "JavaScript wasn't always called that. What other name has it been released under?",
+        choices: [{ text: "Latte", correct: "false" }, { text: "Mocha", correct: "true" }, { text: "BScript", correct: "false" }, { text: "Spidermonkey", correct: false }],
+
+    },
+    {
+        question: "Which tool can you use to ensure code quality?",
+        choices: [{ text: "Angular", correct: "false" }, { text: "jQuery", correct: "" }, { text: "RequireJS", correct: "false" }, { text: "ESLint", correct: "true" }],
+    }
+    ];
